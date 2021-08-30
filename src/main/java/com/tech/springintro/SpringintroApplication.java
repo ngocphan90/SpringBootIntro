9477c5eb2617cd966dc60eb125531a9d8adf08a9
ngocphan90 <bichngoc0803@yahoo.com>
2021-08-29 19:44:34 -0500

    HelloWorld Angular

--- src/main/java/com/tech/springintro/SpringintroApplication.java
+++ src/main/java/com/tech/springintro/SpringintroApplication.java
@@ -1,7 +1,14 @@
 package com.tech.springintro;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringintroApplication {
@@ -12,3 +19,9 @@ public class SpringintroApplication {
 
 
 }
+
+
+
+
+
+
--- src/main/java/com/tech/springintro/controller/PersonController.java
+++ src/main/java/com/tech/springintro/controller/PersonController.java
@@ -5,7 +5,7 @@ import com.tech.springintro.data.PersonRepository;
 import com.tech.springintro.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -17,7 +17,7 @@ public class PersonController {
         return personRepository.getPersonById(id);
 
     }
-    @PutMapping("/addNewPerson")
+    @PostMapping("/addNewPerson")
     public String addNewPerson(@RequestBody Person person) {
         personRepository.save(person);
 
